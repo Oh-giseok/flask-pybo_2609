8f4e8997ab802195a54f6fd473b37fce8323c966
Ohgiseok <ogs4777@gmail.com>
2026-09-16 13:11:18 +0900

    0916 : flask 수업 중

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,17 +1,24 @@
 from datetime import datetime
-from flask import Blueprint, request, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, render_template
 from pybo import db
+from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['POST'])
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
 
-    answer = Answer(question=question, content=content, create_date=datetime.now())
-    db.session.add(answer)
-    db.session.commit()
+    if form.validate_on_submit():
+        # content = request.form['content'] # 폼태그 입력된 값 받을 때
+        content = form.content.data         # 폼모듈로 입력된 값 받을때
 
-    return redirect(url_for('question.detail', question_id=question.id))
+        answer = Answer(question=question, content=content, create_date=datetime.now())
+        db.session.add(answer)
+        db.session.commit()
+
+        return redirect(url_for('question.detail', question_id=question.id))
+
+    return render_template('question/question_detail.html', question=question, form=form)
